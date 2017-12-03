3b9edc40f11c669c07211b7dcfea3f6a4edd4fd9
tiJy <johnsonly2002@hotmail.com>
2017-12-04 01:52:12 +1100

    Final Commit (Pre-Jam)

---
+++ Assets/TriggerDialogueCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogueCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.tag == "Player")
+		{
+			print("Contact!");
+			GetComponent<DialogueTrigger>().TriggerDialogue();
+		}
+	}
+}
